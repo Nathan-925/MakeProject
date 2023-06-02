a876904dfd7d9547e7238163a017912f4b967767
Nathan <61805267+Nathan-925@users.noreply.github.com>
2023-06-02 15:33:26 -0500

    Add files via upload

--- makeproject.sh
+++ makeproject.sh
@@ -2,21 +2,21 @@
 
 # On my honor:
 #
-# - I have not discussed the C language code in my program with
+# - I have not discussed my bash script with
 # anyone other than my instructor or the teaching assistants
 # assigned to this course.
 #
-# - I have not used C language code obtained from another student,
+# - I have not used bash scripts obtained from another student,
 # the Internet, or any other unauthorized source, either modified
 # or unmodified.
 #
-# - If any C language code or documentation used in my program
+# - If any bash script or documentation used in my program
 # was obtained from an authorized source, such as a text book or
 # course notes, that has been clearly noted with a proper citation
-# in the comments of my program.
+# in the comments of my script.
 #
-# - I have not designed this program in such a way as to defeat or
-# interfere with the normal operation of the Curator System.
+# - I have not designed this script in such a way as to defeat or
+# interfere with normal grading.
 #
 # Nathaniel Dunlap
 
